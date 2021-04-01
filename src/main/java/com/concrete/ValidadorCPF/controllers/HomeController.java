d8dd1fa1e214197438885c987f8a557741e93dfe
Pedro <53277438+pdalston@users.noreply.github.com>
2021-03-31 21:39:42 -0300

    Adicionando ajustes nos arquivos html e css

--- src/main/java/com/concrete/ValidadorCPF/controllers/HomeController.java
+++ src/main/java/com/concrete/ValidadorCPF/controllers/HomeController.java
@@ -3,7 +3,7 @@ package com.concrete.ValidadorCPF.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 import com.concrete.ValidadorCPF.models.Cliente;
 
